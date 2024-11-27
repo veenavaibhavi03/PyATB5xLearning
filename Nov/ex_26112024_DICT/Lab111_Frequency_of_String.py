0ed500150eb55da23b04778340e28af5e5dc519d
veenavaibhavi03 <pendyalavaibhavi3@gmail.com>
2024-11-27 23:23:50 +0100

    27Nov

--- Nov/ex_26112024_DICT/Lab111_Frequency_of_String.py
+++ Nov/ex_26112024_DICT/Lab111_Frequency_of_String.py
@@ -1,1 +1,15 @@
 #find frequency of a letter in a string
+string_input="Helloworld"
+list_result=list(string_input)
+print(list_result)
+print(len(list_result))
+a_dict={}
+count=0
+"""for i in list_result:
+    count=list_result.count(i)
+    print(i,count)"""
+
+for i in string_input:
+    a_dict[i]=a_dict.get(i,0)+1
+
+print(a_dict)
